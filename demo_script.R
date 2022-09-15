ee266bea3cfc1d50f764bd7a63ee1943df39b6ff
Santhosigan Thamotharampillai <santhosigan.thamotharampillai@student.nhh.no>
2022-09-15 09:19:44 +0200

    adding the crazy stuff

--- demo_script.R
+++ demo_script.R
@@ -1,17 +1,16 @@
 # Headline 
 
-<<<<<<< HEAD
+
 x <- rnorm(100)
 y <- rnorm(150)
 
 # Important feature 
 
 # Ne feature
-=======
+
 x <- rnorm(200)
 y <- rnorm(300)
 
 # Important feature 
 
-# Crazy idea
->>>>>>> crazy-idea
+# lowrisk crazy stuff
