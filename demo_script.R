edd58f54748460a219a638b92f91f9edf7dab305
Santhosigan Thamotharampillai <santhosigan.thamotharampillai@student.nhh.no>
2022-09-15 09:21:05 +0200

    New master

--- demo_script.R
+++ demo_script.R
@@ -1,17 +1,16 @@
 # Headline 
 
-<<<<<<< HEAD
 x <- rnorm(100)
-y <- rnorm(150)
+y <- rnorm(100)
 
 # Important feature 
 
 # Ne feature
-=======
+
 x <- rnorm(200)
 y <- rnorm(300)
 
 # Important feature 
 
 # Crazy idea
->>>>>>> crazy-idea
+
